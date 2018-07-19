f325df6d6f41b0b00b9f9338b2f1d11ce41de852
Hungter007 <601317127@qq.com>
2018-07-19 22:10:07 +0800

    commit all file

---
+++ apps/organization/models.py
@@ -0,0 +1,49 @@
+# _*_coding=UTF-8_*_
+from datetime import datetime
+
+from django.db import models
+
+# Create your models here.
+
+
+class CityDict(models.Model):
+    name = models.CharField(max_length=20, verbose_name=u"城市")
+    desc = models.CharField(max_length=200, verbose_name=u"描述")
+    add_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = "城市"
+        verbose_name_plural = verbose_name
+
+
+class CourseOrg(models.Model):
+    name = models.CharField(max_length=20, verbose_name=u"机构名称")
+    des = models.TextField(verbose_name=u"机构描述")
+    click_nums = models.IntegerField(default=0, verbose_name=u"点击数")
+    fav_nums = models.IntegerField(default=0, verbose_name=u"收藏数")
+    address = models.CharField(max_length=100, verbose_name=u"机构地址")
+    image = models.ImageField(upload_to="org/%Y/%m", max_length=100, verbose_name=u"图标")
+    city = models.ForeignKey(CityDict, verbose_name=u"所在城市")
+    add_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = "课程机构"
+        verbose_name_plural = verbose_name
+
+
+class Teacher(models.Model):
+    org = models.ForeignKey(CourseOrg, verbose_name=u"所属机构")
+    name = models.CharField(max_length=20, verbose_name=u"教师名称")
+    work_yaers = models.IntegerField(default=0, verbose_name=u"工作年限")
+    work_company = models.CharField(max_length=50, verbose_name=u"就职公司")
+    work_position = models.CharField(max_length=50, verbose_name=u"公司职位")
+    points = models.CharField(max_length=20, verbose_name=u"教学特点")
+    age = models.CharField(max_length=20, verbose_name=u"年龄")
+    click_nums = models.IntegerField(default=0, verbose_name=u"点击数")
+    fav_nums = models.IntegerField(default=0, verbose_name=u"收藏数")
+    add_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = "教师信息"
+        verbose_name_plural = verbose_name
+
